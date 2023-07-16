0ea223302fef61695fdd106d74d3e2b9d6ff0b07
Karine <karine.godelar@gmail.com>
2023-07-16 16:22:05 +0200

    wip

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/'
-import Survey from './pages/Survey'
+import Survey from './pages/Survey/'
 import Header from './components/Header'
 
 ReactDOM.render(
